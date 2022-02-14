cbbfabce7900fd478409b900d38a33d6ac2e4102
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-14 17:25:44 +0530

    added ec2 instance creation

---
+++ Lab-3/c1-versions.tf
@@ -0,0 +1,16 @@
+#Terraform block
+terraform {
+  # reffered to cloud vendors or any provide you can have
+  required_providers {
+    aws={
+        source ="hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+   region="us-east-1"
+   profile="default"
+}
+
+
+
