1086634401e58c1bfbb9e7ba87949a674ab86d0e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-17 15:01:10 +0530

    state file

---
+++ Lab-19/c1-versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version="3.0"
+    }
+  }
+  #adding backend aas s3 remote state storage
+  backend "s3" {
+    bucket = "terraform-backend-sample-551"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+    #enable state locking
+    #for state locking
+    dynamodb_table = "terraform-dev-state-table"
+    
+  }
+
+}
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+
+
