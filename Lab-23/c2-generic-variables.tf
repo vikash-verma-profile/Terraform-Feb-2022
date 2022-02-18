87ac978a96e5b8ffefbd416d5125dda2989112f8
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-18 09:36:18 +0530

    workspace

---
+++ Lab-23/c2-generic-variables.tf
@@ -0,0 +1,61 @@
+
+#business Unit
+variable "business_unit"{
+  description="Business Unit Name"
+  type = string
+  default = "hr"
+}
+
+#Environment Name
+variable "environment"{
+  description="Environment Name"
+  type = string
+  default = "dev"
+}
+#Resource Group Name
+variable "resource_group_name"{
+  description="Resource Group Name"
+  type = string
+  default = "myreg"
+}
+#Resource Group Location
+
+# == !=  || or && AND 
+/*(expression one)||(expression two) if any of them is true it will work 
+if both are false then it will given false
+true || true final result true
+false || true final result true
+true || false final result true
+false || false final result false
+
+for AND
+
+true && true final result true
+false && true final result false
+true && false final result false
+false && false final result false
+
+*/
+variable "resource_group_location" {
+  description="Resource Group Location"
+  type = string
+  default = "eastus"
+}
+#Virtual Network Name
+variable "virtual_network_name" {
+  description="Virtual Network Name"
+  type = string
+  default = "myvnet"
+}
+#virtual network address -dev
+variable "vnet_address_space_dev" {
+  description="Virtual Network Address Space for Dev Environment"
+  type = list(string)
+  default = ["10.0.0.0/16"]
+}
+#virtual network address -All
+variable "vnet_address_space_all" {
+  description="Virtual Network Address Space for All Environment except dev"
+  type = list(string)
+  default = ["10.0.0.0/16","10.2.0.0/16","10.3.0.0/16"]
+}
